92b6036a5ea21ed2ef4e38212558c28123ff59ee
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-16 23:53:33 -0300

    Create scripts for db migration and first migration again

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, primary_key=True, index=True)
     name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
