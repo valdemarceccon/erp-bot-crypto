b7ce95064b3cb5e274b8f331308d33b37fd32669
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-17 14:40:24 -0300

    rbac and admin role and permission

---
+++ backend/alembic/versions/2023_04_17_1429-7b74c7966839_create_initial_admin_role.py
@@ -0,0 +1,29 @@
+"""Create initial admin role
+
+Revision ID: 7b74c7966839
+Revises: 6fefa0b968eb
+Create Date: 2023-04-17 14:29:37.414844
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import text
+
+
+# revision identifiers, used by Alembic.
+revision = "7b74c7966839"
+down_revision = "6fefa0b968eb"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # ...
+    connection = op.get_bind()
+    connection.execute(text("INSERT INTO roles (name) VALUES ('admin');"))
+
+
+def downgrade():
+    # ...
+    connection = op.get_bind()
+    connection.execute(text("DELETE FROM roles WHERE name = 'admin';"))
