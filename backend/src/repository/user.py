a127c05d3bca1da8df81ee667790bd3cf4ee69a7
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-17 01:37:34 -0300

    This should be a str already, but type check says it isn't

--- backend/src/repository/user.py
+++ backend/src/repository/user.py
@@ -25,6 +25,7 @@ def authenticate_user(db: Session, email: str, password: str) -> User | None:
     user: User = db.query(User).filter(User.email == email).first()
     if not user:
         return None
-    if not verify_password(password, user.hashed_password):
+
+    if not verify_password(password, str(user.hashed_password)):
         return None
     return user
