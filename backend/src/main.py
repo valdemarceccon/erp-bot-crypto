8641620c4efbc610ab0c1cc6b50bce8de42282de
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-17 00:16:39 -0300

    Health check with examples for external dependencies (like apis)

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,55 @@
 from fastapi import FastAPI
+from .models.base import engine
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import text
+
+# import httpx
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+app = FastAPI()
+
+
+# EXTERNAL_API_URL = "https://example.com/api/health"
+
+
+@app.get("/health")
+async def health_check():
+    health_status = {
+        "database": "unknown",
+        # "external_api": "unknown",
+    }
+
+    # Check the database connection
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        health_status["database"] = "OK"
+    except Exception as e:
+        health_status["database"] = "unavailable"
+
+    # Check the external API
+    # try:
+    #     async with httpx.AsyncClient() as client:
+    #         response = await client.get(EXTERNAL_API_URL)
+    #         response.raise_for_status()
+    #     health_status["external_api"] = "OK"
+    # except Exception as e:
+    #     health_status["external_api"] = "unavailable"
+
+    if all(value == "OK" for value in health_status.values()):
+        return health_status
+    else:
+        raise HTTPException(status_code=500, detail=health_status)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(host="0.0.0.0", port=8000)
