63ca82bef8f2a9464c20d2a649a08511b2a47875
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-17 02:42:18 -0300

    Some bug when creating migrations

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -4,6 +4,8 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from src.models.base import Base
 
+# from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
@@ -12,3 +14,15 @@ class User(Base):
     email = Column(String, primary_key=True, index=True)
     name = Column(String, index=True)
     hashed_password = Column(String)
+
+    # apikeys = relationship('APIKey', backref='user')
+
+
+class APIKey(Base):
+    __tablename__ = "apikeys"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+    apikey = Column(String, nullable=False)
+
+    # user = relationship('User', backref='apikeys')
