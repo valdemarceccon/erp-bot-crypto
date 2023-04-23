ee815fa4209d580526655326684e723103a7cf72
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-23 11:53:05 -0300

    Update alembic script

--- backend/db_auto_gen.sh
+++ backend/db_auto_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH="$(pwd)/venv/bin:$PATH"
-docker-compose exec backend alembic revision -m "$*"
+docker-compose exec backend alembic revision --autogenerate -m "$*"
 chown -R $(id -u):$(id -g) alembic
