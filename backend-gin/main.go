5eefdf488fea752579122fa1463a953eb1312c16
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2023-04-28 23:00:32 -0300

    Trying gin golang

---
+++ backend-gin/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pog",
+		})
+	})
+	r.Run("0.0.0.0:3000")
+}
